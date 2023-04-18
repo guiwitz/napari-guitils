db870641fdcfe40cc939f6c99bac0b6ba1a827b1
guiwitz <guiwitz@users.noreply.github.com>
2023-04-18 13:01:01 +0200

    multitab layout

--- src/napari_guitils/gui_elements.py
+++ src/napari_guitils/gui_elements.py
@@ -1,15 +1,55 @@
-from qtpy.QtWidgets import (QTabWidget, QWidget, QVBoxLayout)
+from qtpy.QtWidgets import (QTabWidget, QWidget, QVBoxLayout,
+                            QHBoxLayout, QGridLayout, QGroupBox)
 
 
-def create_tabs(tab_names, main_layout):
+class TabSet(QTabWidget):
+    """Multi-tab widget with named tabs.
+    
+    Parameters
+    ----------
+    tab_names : list of str
+        The names of the tabs to create.
+    tab_layouts : list of layouts, optional
+        box layouts to use e.g QVBoxLayout, QHBoxLayout, QGridLayout, optional
+
+    """
+    def __init__(self, tab_names, tab_layouts=None):
+        super().__init__()
+
+        self.tab_names = tab_names
+        tab_widgets = [QWidget() for _ in tab_names]
+        if tab_layouts is None:
+            tab_layouts = [None for _ in tab_names]
+        tab_layouts = [QVBoxLayout() if tl is None else tl for tl in tab_layouts]
+        
+        for t_layout, t_widget, t_name in zip(tab_layouts, tab_widgets, tab_names):
+            t_widget.setLayout(t_layout)
+            self.addTab(t_widget, t_name)
+
+    def add_named_tab(self, tab_name, widget):
+        """Add a widget to a named tab.
+        
+        Parameters
+        ----------
+        tab_name : str
+            The name of the tab to add the widget to.
+        widget : QWidget
+            The widget to add to the tab.
+
+        """
+        
+        self.widget(self.tab_names.index(tab_name)).layout().addWidget(widget)
+
+
+def create_tabs(tab_names, tab_layouts=None):
     """Create a tab widget with the given tab names.
 
     Parameters
     ----------
     tab_names : list of str
         The names of the tabs to create.
-    main_layout : QWidget, optional
-        main layout to add the tabs to
+    tab_layouts : list of Layouts, optional 
+        layouts to use e.g QVBoxLayout, QHBoxLayout, QGridLayout, optional
 
     Returns
     -------
@@ -19,12 +59,14 @@ def create_tabs(tab_names, main_layout):
     
     tabs = QTabWidget()
     tab_widgets = [QWidget() for _ in tab_names]
-    tab_layouts = [QVBoxLayout() for _ in tab_names]
+    if tab_layouts is None:
+        tab_layouts = [None for _ in tab_names]
+    tab_layouts = [QVBoxLayout() if tl is None else tl for tl in tab_layouts]
+    
     for t_layout, t_widget, t_name in zip(tab_layouts, tab_widgets, tab_names):
         t_widget.setLayout(t_layout)
         tabs.addTab(t_widget, t_name)
     
-    #main_layout.addWidget(tabs)
     return tabs
 
 class VHGroup():
